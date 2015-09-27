8ab896bb071ef11dd77e8ab2542084d72fc70f34
rsarten <rorysarten@gmail.com>
2015-09-27 20:41:24 +1300

    Created cacheMatrix.R script for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions calculate and caches the inverse of a matrix 'x'
+##
+## The makeCacheMatrix function creates ('set') the matrix object and has the functions
+## to retrieve ('get') 'x', cache the inverse of 'x' ('setinverse'), and to retrieve the
+## cached inverse ('getinverse').
+##
+## The cacheSolve function looks for the cached inverse, and returns it if it exits
+## or creates and caches it otherwise, also returning it after.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#makeCacheMatrix creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()){
+  
+  inverse <- NULL # Sets inverse to NULL when function initiated
+  
+  set <- function(y){
+    x <<- y
+    inverse <<- NULL # clears cached inverse with new matrix set
+  }
+  get <- function() x # Function for retrieving matrix 'x'
+  setinverse <- function(inv) inverse <<- inv # function for caching inverse
+  getinverse <- function() inverse # function for retrieving cached inverse
+  
+  #returns list of functions relating to matrix
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
-
+#cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inverse <- x$getinverse() # Look for cached inverse
+  if (!is.null(inverse)) {  # If one exists return it
+    message("getting inverted matrix")
+    return(inverse)
+  }
+  matrix <- x$get()         # If no cached inverse, retrieve matrix
+  inverse <- solve(matrix)  # Calculate inverse matrix
+  x$setinverse(inverse)     # Cache inverse
+  inverse # Return a matrix that is the inverse of 'x'
 }
